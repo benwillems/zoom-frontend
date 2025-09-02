a1ba7909f14827a32c8c5cbe64ce995fb3ab4c96
Ben <willems.ben@gmail.com>
2025-09-02 14:44:49 -0400

    Update custom-logout.js

--- src/pages/api/auth/custom-logout.js
+++ src/pages/api/auth/custom-logout.js
@@ -3,12 +3,12 @@ import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0'
 export default withApiAuthRequired(async function logout(req, res) {
   try {
     const session = await getSession(req, res)
-    if (session) {
+   // if (session) {
       res.setHeader(
         'Set-Cookie',
         'appSession=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Lax'
       )
-    }
+   // }
     res.status(200).json({ success: true })
   } catch (error) {
     console.error(error)
